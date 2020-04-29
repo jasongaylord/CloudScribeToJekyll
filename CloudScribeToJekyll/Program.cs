8a42f2ea10a32ba139e1dac3adf265e9e20b88c3
Jason Gaylord <Jason.Gaylord@biberk.com>
2020-04-29 17:18:42 -0400

    Added class objects and started the SQLread

--- CloudScribeToJekyll/Program.cs
+++ CloudScribeToJekyll/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
+using System.Text;
 
 namespace CloudScribeToJekyll
 {
@@ -20,6 +22,36 @@ namespace CloudScribeToJekyll
                 Password = config.password,
                 InitialCatalog = config.database
             };
+
+            // Open sql conneciton to build post object
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                // Build post query
+                var post_sql_sb = new StringBuilder();
+                post_sql_sb.Append("SELECT p.Id, p.Title, p.Slug, p.PubDate, p.IsPublished, p.CategoriesCsv, p.Content, u.DisplayName, u.Email");
+                post_sql_sb.Append("FROM cs_Post p LEFT JOIN cs_User u ON p.Author = u.Email");
+
+                if (!string.IsNullOrEmpty(config.blogid))
+                    post_sql_sb.Append("WHERE p.BlogId = '" + config.blogid + "'");
+
+                var post_sql = post_sql_sb.ToString();
+
+                // Read each post
+                using (var post_command = new SqlCommand(post_sql, connection))
+                {
+                    using (var post_reader = post_command.ExecuteReader())
+                    {
+                        while (post_reader.Read())
+                        {
+
+                        }
+                    }
+                }
+
+                // Next
+            }
         }
     }
 
@@ -33,5 +65,29 @@ namespace CloudScribeToJekyll
         public string blogid { get; set; }
         public bool comments { get; set; }
         public bool categories { get; set; } 
+    }
+
+    public class Post
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string slug { get; set; }
+        public string pubdate { get; set; }
+        public bool isPublished { get; set; }
+        public string categoriescsv { get; set; }
+        public string content { get; set; }
+        public string displayname { get; set; }
+        public string email { get; set; }
+        public List<Comment> comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public string id { get; set; }
+        public string author { get; set; }
+        public string email { get; set; }
+        public string website { get; set; }
+        public string pubdate { get; set; }
+        public string content { get; set; }
     }
 }

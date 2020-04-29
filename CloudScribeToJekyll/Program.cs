b25f5f7a3717469d7ad8b6277da7a10372b3a732
Jason Gaylord <Jason.Gaylord@biberk.com>
2020-04-29 16:48:52 -0400

    Converted the JSON file to an object. Created the SQL connection.

--- CloudScribeToJekyll/Program.cs
+++ CloudScribeToJekyll/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace CloudScribeToJekyll
 {
@@ -6,7 +9,29 @@ namespace CloudScribeToJekyll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Read configuration file
+            var config = JsonConvert.DeserializeObject<CloudScribeSettings>(File.ReadAllText("cloudscribe.json"));
+
+            // Create sql connection
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = config.server,
+                UserID = config.username,
+                Password = config.password,
+                InitialCatalog = config.database
+            };
         }
+    }
+
+    public class CloudScribeSettings
+    {
+        public string server { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string database { get; set; }
+        public string filetype { get; set; }
+        public string blogid { get; set; }
+        public bool comments { get; set; }
+        public bool categories { get; set; } 
     }
 }
